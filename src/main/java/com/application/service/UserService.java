55033101a03088d92a5fc05f6527f984131a2d33
prasanna <prasanna0614@gmail.com>
2020-05-08 15:15:24 +0530

    done changes for web service to integrate

--- src/main/java/com/application/service/UserService.java
+++ src/main/java/com/application/service/UserService.java
@@ -4,22 +4,35 @@ import com.application.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class UserService {
+
+
+
+    @GetMapping(path="Hello/path-variable/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public  String getProducts(@PathVariable String name) {
+        User user = new User();
+        user.setName(name);
+        user.setPassword(name);
+        String message="password verified for ".concat(String.format(name).concat(" in the web service"));
+                return message;
+
+
+    }
+
+
 
     @RequestMapping(value = "/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
     @RequestMapping(value = "/api/authenticateUser",
-    method = RequestMethod.POST,
-    produces = MediaType.APPLICATION_JSON_VALUE,
-    consumes = MediaType.APPLICATION_JSON_VALUE)
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<User> validateUser(@RequestBody User  user) {
         if(user.getName().equalsIgnoreCase("pioneers")) {
             return new ResponseEntity<User>(user, HttpStatus.OK);
@@ -27,13 +40,4 @@ public class UserService {
         return new ResponseEntity<User>(user, HttpStatus.BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/api/getUser",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> getUser() {
-        User user = new User();
-        user.setName("pioneers");
-        user.setPassword("name");
-        return new ResponseEntity<User>(user, HttpStatus.OK);
-    }
 }
